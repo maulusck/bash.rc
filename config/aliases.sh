8d30f266eadc9934625727cc2e0dcad02dcaf84a
Maulusck <maulusck@pm.me>
2024-11-16 14:51:12 +0100

    cleaned up aliases

--- config/aliases.sh
+++ config/aliases.sh
@@ -2,48 +2,49 @@
 
 # BEGIN ALIASES
 
-# edit this file
-alias aliases='sudo nano /etc/bash/bashrc.d/aliases.sh'
+# edit aliases
+[ $(id -u) -eq 0 ] && export aliases_file="/etc/bash/bashrc.d/custom.sh" || export aliases_file="$HOME/.bashrc"
+alias aliases="touch $aliases_file ; test -w $aliases_file && nano $aliases_file && source $aliases_file"
 
 # enable color support of ls and also add handy aliases
-alias ls='ls --color=auto'
-alias dir='dir --color=auto'
-alias vdir='vdir --color=auto'
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
+alias ls="ls --color=auto"
+alias dir="dir --color=auto"
+alias vdir="vdir --color=auto"
+alias grep="grep --color=auto"
+alias fgrep="fgrep --color=auto"
+alias egrep="egrep --color=auto"
 # some more ls aliases
-alias ll='ls -lh'
-alias la='ls -lha'
-alias l='ls -CF'
-alias lisa='ls -lisa'
-alias dd='dd status=progress'
-alias _='sudo'
-alias _i='sudo -i'
-alias _I='sudo -i'
+alias ll="ls -lh"
+alias la="ls -lha"
+alias l="ls -CF"
+alias lisa="ls -lisa"
+alias dd="dd status=progress"
+alias _="sudo"
+alias _i="sudo -i"
+alias _I="sudo -i"
 
 #  CUSTOM ALIASES
 
-alias q='exit'
-alias cl='clear'
-alias rm='rm -i'
-alias ls..='ls ..'
-alias cd..='cd ..'
-alias cd-='cd -'
-alias off='poweroff'
-alias cl='clear'
-alias ipa='ip a'
-alias ipr='ip r'
-alias tmuxa='tmux attach'
+alias q="exit"
+alias cl="clear"
+alias rm="rm -i"
+alias ls..="ls .."
+alias cd..="cd .."
+alias cd-="cd -"
+alias off="poweroff"
+alias cl="clear"
+alias ipa="ip a"
+alias ipr="ip r"
+alias tmuxa="tmux attach"
 alias _="sudo"
-alias please='sudo'
-alias fucking='sudo'
-alias _i='sudo su -'
-alias x='startx'
-alias bat='batcat'
+alias please="sudo"
+alias fucking="sudo"
+alias _i="sudo su -"
+alias x="startx"
+alias bat="batcat"
 
 # infallible wget
-alias wget='wget -c -t 0 -T 15'
+alias wget="wget -c -t 0 -T 15"
 
 # functions
 
@@ -51,3 +52,4 @@ alias wget='wget -c -t 0 -T 15'
 function loop(){
         while true; do (${@:2} && sleep $1 && clear) done
 }; export -f loop
+
--- config/aliases_custom.sh
+++
@@ -1,9 +0,0 @@
-# /etc/bash/bashrc.d/aliases_custom.sh
-
-# machine-specific aliases
-
-# EXAMPLE - apple ascii art
-alias neofetch='neofetch --ascii_distro macos'
-
-# EXAMPLE - tty-clock
-alias clock='tty-clock -sScC 4'
